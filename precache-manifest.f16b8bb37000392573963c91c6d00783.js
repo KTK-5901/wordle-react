1d07db360b7829a9e7e2f3ee787bd7dd28e3b378
KTK-5901 <kartikeygupta59@gmail.com>
2022-05-16 03:00:06 +0530

    Updates

---
+++ precache-manifest.f16b8bb37000392573963c91c6d00783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8266d2d6b5c2f7bf993",
+    "url": "/wordle-react/static/css/main.d4a4bd91.chunk.css"
+  },
+  {
+    "revision": "a8266d2d6b5c2f7bf993",
+    "url": "/wordle-react/static/js/main.a8266d2d.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/wordle-react/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "a43f66027e93429f0c4e",
+    "url": "/wordle-react/static/js/2.a43f6602.chunk.js"
+  },
+  {
+    "revision": "af87ddb914be662c4559",
+    "url": "/wordle-react/static/js/runtime~main.af87ddb9.js"
+  },
+  {
+    "revision": "44a432e2857f632df811dca836916c74",
+    "url": "/wordle-react/static/media/wordle-bank.44a432e2.txt"
+  },
+  {
+    "revision": "19001faf0c7982bf8e76c620cb0ef33b",
+    "url": "/wordle-react/index.html"
+  }
+];
